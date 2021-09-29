d7b9874bfc6d924aa55126e94840efcb5147ccab
konstantin <kosval09@gmail.com>
2021-09-29 23:13:09 +0900

    пример частного измениемого свойства в ViewModel

--- app/src/main/java/ru/kostry/app/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/kostry/app/viewmodel/MainViewModel.kt
@@ -1,7 +1,17 @@
 package ru.kostry.app.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+//пример частного измениемого свойства
+    private val _num = MutableLiveData<Int>(0)
+    val num: LiveData<Int> = _num
+
+//    способ изменения частного изменяемого свойства
+    fun setNum(numbers: Int) {
+        _num.value = numbers
+    }
 }
