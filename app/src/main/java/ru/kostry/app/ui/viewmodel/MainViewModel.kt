03e6bb03f35320dfdf77f5ae481219340e5663e9
konstantin <kosval09@gmail.com>
2021-10-07 01:17:41 +0900

    изменен MainViewModel.kt

--- app/src/main/java/ru/kostry/app/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/kostry/app/ui/viewmodel/MainViewModel.kt
@@ -3,8 +3,22 @@ package ru.kostry.app.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.kostry.app.network.MarsApi
+import ru.kostry.app.network.MarsPhoto
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 class MainViewModel : ViewModel() {
+
+    //понадобится для логики показа успеха загрузки
+    private val _status = MutableLiveData<MarsApiStatus>()
+    val status: LiveData<MarsApiStatus> = _status
+
+    //    список объектов MarsPhoto
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
 
     private val _myNumber = MutableLiveData<Int>()
     val myNumber: LiveData<Int> = _myNumber
@@ -14,6 +28,22 @@ class MainViewModel : ViewModel() {
 
     init {
         resetModel()
+        getMarsPhotos()
+    }
+
+
+//    получить фото из MarsApi.retrofitService
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
     }
 
     fun resetModel() {
