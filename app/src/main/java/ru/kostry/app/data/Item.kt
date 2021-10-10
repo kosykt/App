0a04bc8cb99ce4131150a2a1eb4b7bbc113e26f5
konstantin <kosval09@gmail.com>
2021-10-10 18:36:35 +0900

    добавлен Item

---
+++ app/src/main/java/ru/kostry/app/data/Item.kt
@@ -0,0 +1,13 @@
+package ru.kostry.app.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val name: String
+)
