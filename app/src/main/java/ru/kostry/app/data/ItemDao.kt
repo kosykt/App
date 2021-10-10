d0ee7705708c454102d86324a0e88144fd99f052
konstantin <kosval09@gmail.com>
2021-10-10 18:56:18 +0900

    добавлен ItemDao

---
+++ app/src/main/java/ru/kostry/app/data/ItemDao.kt
@@ -0,0 +1,12 @@
+package ru.kostry.app.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+}
